a5c164d2addf453e9cf9b3cf3c2c799b2cfe059e
Alejandro Torres <at7709739@gmail.com>
2025-11-03 00:31:45 -0300

    feat: implmentacion de clase LectorConsola

---
+++ src/main/java/com/chadslab/utils/entradautils/LectorConsola.java
@@ -0,0 +1,46 @@
+package com.chadslab.utils.entradautils;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class LectorConsola {
+    private Scanner scanner;
+
+    public LectorConsola() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int leerInt(String mensaje) {
+        while (true) {
+            try {
+                System.out.println(mensaje);
+                int numero = Integer.parseInt(scanner.nextLine());
+
+                return numero;
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Por favor ingrese un número válido");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    public String leerString(String mensaje) {
+        System.out.println(mensaje);
+        return scanner.nextLine();
+    }
+
+    public List<Integer> leerIntLista(String mensaje) {
+        List<Integer> lista = new ArrayList<>();
+        System.out.println(mensaje);
+        while (true) {
+            int num = leerInt("Id: ");
+            if (num == -1) {
+                break;
+            }
+            lista.add(num);
+        }
+        return lista;
+    }
+}
