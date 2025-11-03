08eb342968d0705d8d3b1f83a3e41b860d900690
Alejandro Torres <at7709739@gmail.com>
2025-11-03 00:31:16 -0300

    feat: implmentacion de clase impresora

---
+++ src/main/java/com/chadslab/utils/salidautils/Impresora.java
@@ -0,0 +1,12 @@
+package com.chadslab.utils.salidautils;
+
+public interface Impresora {
+
+    void mostrarReporteSimple();
+
+    void mostrarExitososYFallidos();
+
+    void mostrarInvestigadorConMasExperimentos();
+
+    void mostrarExperimentosPorTipoYResultado();
+}
---
+++ src/main/java/com/chadslab/utils/salidautils/impl/ImpresoraImpl.java
@@ -0,0 +1,59 @@
+package com.chadslab.utils.salidautils.impl;
+
+import com.chadslab.dominio.Experimento;
+import com.chadslab.dominio.Investigador;
+import com.chadslab.service.experimento.ExperimentoService;
+import com.chadslab.service.investigador.InvestigadorService;
+import com.chadslab.utils.salidautils.Impresora;
+
+import java.util.List;
+
+public class ImpresoraImpl implements Impresora {
+
+    private ExperimentoService experimentoService;
+    private InvestigadorService investigadorService;
+
+    public ImpresoraImpl(ExperimentoService experimentoService, InvestigadorService investigadorService) {
+        this.experimentoService = experimentoService;
+        this.investigadorService = investigadorService;
+    }
+
+    @Override
+    public void mostrarReporteSimple() {
+        System.out.println("Reporte Simple de experimentos");
+        System.out.println("Promedio de duracion de los experimentos: " + experimentoService.promedioDuracion());
+        System.out.println("Porcebntaje de éxtio global: " + experimentoService.porcentajeExito());
+        //System.out.println("******************************");
+
+    }
+
+    @Override
+    public void mostrarExitososYFallidos() {
+
+        List<Integer> exitososYFallidos = experimentoService.getExitososYFallidos();
+
+        System.out.println("Experimentos EXISTOSOS y FALLIDOS");
+        System.out.println("Experimentos EXITOSOS:" + exitososYFallidos.getFirst());
+        System.out.println("Experimentos FALLIDOS:" + exitososYFallidos.get(1));
+
+    }
+
+    @Override
+    public void mostrarInvestigadorConMasExperimentos() {
+        Investigador investigadorConMasExperimentos = investigadorService.investigadorConMasExperimentos();
+
+        System.out.println("El investigador con más experimentos es: ");
+        System.out.println(investigadorConMasExperimentos);
+
+    }
+
+    @Override
+    public void mostrarExperimentosPorTipoYResultado() {
+        List<Experimento> experimentos = experimentoService.getExperimentos();
+        System.out.println("Experimentos por Tipo y Resultado");
+        for (Experimento experimento : experimentos) {
+            System.out.println("Resultado: " + experimento.getResultado());
+            System.out.println("Tipo: " + experimento.getClass().getSimpleName());
+        }
+    }
+}
